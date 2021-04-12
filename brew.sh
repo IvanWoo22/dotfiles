782b721be909d9e3c0a16415b3878a06ae93f047
-IvanWoo- <33291886+IvanWoo22@users.noreply.github.com>
2021-04-12 16:46:39 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -24,4 +24,4 @@ brew install gsettings-desktop-schemas gtk+3 adwaita-icon-theme
 
 # bio
 brew tap brewsci/bio
-brew install bbtools blast bowtie2 bwa cutadapt fastqc figtree freebayes gatk hisat2 kmergenie mash meme picard-tools pymol repeatmasker samtools
+brew install bbtools blast bowtie2 bwa cutadapt fastqc figtree freebayes gatk hisat2 kmergenie mash meme picard-tools pymol repeatmasker samtools sratoolkit
